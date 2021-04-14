70ab72c7e1b29a485fca10247b0eaab4acb81ac0
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-04-13 20:03:27 -0400

    Add routes, create and show work

---
+++ app/models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  list: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Post', postSchema)
